4e6eaf0069bfa26ac6d6cc62253363a520e7e9f0
Aleš <foxxcz@gmail.com>
2024-03-07 16:48:10 +0100

    config

--- wp-config.php
+++ wp-config.php
@@ -1,96 +1,52 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
-
-/** Database username */
-define( 'DB_USER', 'username_here' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'password_here' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
+// database
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
+define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
 define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+$table_prefix = getenv('WORDPRESS_TABLE_PREFIX');
 
-/**#@-*/
+// auth
+define( 'AUTH_KEY', getenv('WORDPRESS_AUTH_KEY') );
+define( 'SECURE_AUTH_KEY', getenv('WORDPRESS_SECURE_AUTH_KEY') );
+define( 'LOGGED_IN_KEY', getenv('WORDPRESS_LOGGED_IN_KEY') );
+define( 'NONCE_KEY', getenv('WORDPRESS_NONCE_KEY') );
+define( 'AUTH_SALT', getenv('WORDPRESS_AUTH_SALT') );
+define( 'SECURE_AUTH_SALT', getenv('WORDPRESS_SECURE_AUTH_SALT') );
+define( 'LOGGED_IN_SALT', getenv('WORDPRESS_LOGGED_IN_SALT') );
+define( 'NONCE_SALT', getenv('WORDPRESS_NONCE_SALT') );
 
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
+// settings
+define('WP_SITEURL', getenv('WORDPRESS_URL'));
+define('WP_HOME', getenv('WORDPRESS_URL'));
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
+// debug
+define( 'WP_DEBUG', getenv('WORDPRESS_DEBUG') );
+define( 'WP_DEBUG_DISPLAY', getenv('WORDPRESS_DEBUG_DISPLAY') );
 
-/* Add any custom values between this line and the "stop editing" line. */
+// plugins
+define( 'S3_UPLOADS_BUCKET', getenv('WORDPRESS_STORAGE_BUCKET') );
+define( 'S3_UPLOADS_REGION', 'us-east-1' );
+define( 'S3_UPLOADS_KEY', getenv('WORDPRESS_STORAGE_KEY_ID') );
+define( 'S3_UPLOADS_SECRET', getenv('WORDPRESS_STORAGE_ACCESS_KEY') );
+define( 'S3_UPLOADS_BUCKET_URL', rtrim(getenv('WORDPRESS_STORAGE_URL'), '/') . '/' . getenv('WORDPRESS_STORAGE_BUCKET') );
+define( 'S3_UPLOADS_URL', getenv('WORDPRESS_STORAGE_URL') );
+define( 'WP_CACHE', true );
+define( 'WP_REDIS_USER_SESSION_HOST', getenv('WORDPRESS_REDIS_USER_SESSION_HOST') );
 
+// misc
+define('FORCE_SSL_ADMIN', true);
 
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $_SERVER['HTTPS']='on';
+}
 
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-/** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
+require_once ABSPATH . 'wp/wp-settings.php';
